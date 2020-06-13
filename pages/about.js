c947342918a1a0f3f1cab3012db795fd3478a142
Andreazo Silva <andreazocoisas@gmail.com>
2020-06-13 19:58:48 -0300

    modified name file layout

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,19 @@ import Link from 'next/link';
 
 export default function About (){
   return (
-    <div>
+    <div className="container">
       <h1>About page</h1>
       <Link href="/"><a>Home</a></Link>
+
+
+      <style jsx>{`
+        .container {
+          background: #651FFF;
+          height: 100%;
+        }
+      `}</style>
+      
     </div>
   );
 }
+
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 import App from './_app';
-import Layout from '../components/Layout';
+import Layout from '../components/_Layout';
 
 export default function Home() {
   return (
